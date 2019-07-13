bcee54b7b51fb1953686ae5e0a954c0298255775
tyb1222 <tyb1222@gmail.com>
2019-07-13 14:22:48 +0800

    Update SharedLock.java
    增加获取共享锁注释说明

--- src/com/tyb1222/chapt05/SharedLock.java
+++ src/com/tyb1222/chapt05/SharedLock.java
@@ -24,7 +24,9 @@ public class SharedLock implements Lock {
         /**
          * 对共享锁而言，state >0 时，在AQS队列锁中，会传递通知后面所有队列中的节点去获取锁.具体参见 AQS中，doAcquireShared对yryAcquireShared的调用
          * @param count 获取同步状态成功线程个数。每个线程获取成功，则ｃcount=1 。多线程状态下，为保证线程安全，肯定是一个个线程轮流获取.count默认为0
-         * @return 返回小于０，说明当前线程获取同步状态失败；大于0说明获取同步状态成功。AQS模板方法中，tryAcquireShared 返回值大于0 说明获取锁成功，反之失败。下面为代码
+         * @return 返回小于０，说明当前线程获取同步状态失败；大于0说明获取同步状态成功。
+         * 在doAcquireShared 也是通过自旋获取锁
+         *  AQS模板方法中，tryAcquireShared 返回值大于0 说明获取锁成功，反之失败。下面为代码
          *
          *    public final void acquireShared(int arg) {
          *       if (tryAcquireShared(arg) < 0)
